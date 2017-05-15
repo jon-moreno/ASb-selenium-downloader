039954a2a116708c138a3b98271c19690918fdb0
Jonathan Moreno <jonathan.moreno324@gmail.com>
2017-05-15 16:40:06 -0400

    Opens specific FF profile.

--- downloader.py
+++ downloader.py
@@ -58,19 +58,36 @@ books = [
 "Unwise Judge",
 ]
 
-#Opens FF to webpage
-driver = webdriver.Firefox()
+
+#Creates FF profile
+'''
+fxProfile = webdriver.FirefoxProfile()
+
+fxProfile.set_preference("browser.download.folderList",2)
+fxProfile.set_preference("browser.download.manager.showWhenStarting",False)
+fxProfile.set_preference("browser.helperApps.alwaysAsk.force", False);
+fxProfile.set_preference("browser.download.dir","~/Downloads/ASb")
+fxProfile.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv; application/pdf")
+'''
+
+#Opens FF to webpage using profile
+profile = webdriver.FirefoxProfile('/home/jon-moreno/.mozilla/firefox/c7r83610.temp')
+driver = webdriver.Firefox(profile)
 driver.get("http://www.africanstorybook.org/")
 
 #Searches for book
 elem = driver.find_element_by_id("mainSearchInput")
 elem.send_keys(books[0])
-elem.send_keys(Keys.RETURN)
+elem.send_keys(Keys.ENTER)
 
 #Waits until search results loaded
-elem = WebDriverWait(driver, 30).until(
+try:
+    elem = WebDriverWait(driver, 35).until(
         EC.presence_of_element_located((By.ID, "storySearchId"))
     )
+except Exception as e:
+    pass
+
 
 #Opens book searched for
 elem = driver.find_element_by_id("storySearchId")
@@ -86,6 +103,8 @@ elem = WebDriverWait(driver, 30).until(
         #EC.presence_of_element_located((By.CLASS_NAME, "imageBlock0"))
 
     )
+
+'''
 #Opens menu
 elem = driver.find_element_by_class_name("menu-text")
 elem.send_keys(Keys.TAB*2, Keys.ENTER)
@@ -98,6 +117,7 @@ elem = driver.find_element_by_id("accordianRelatedStories")
 
 #Initiates Download
 elem.send_keys(Keys.TAB*50, Keys.ENTER)
+'''
 
 '''So WTF are there 50 tabs? Because you have to tab through the non-visible <li>
 in the menu. You can't click the download <li> directly, otherwise this would work:
@@ -106,6 +126,24 @@ elem = driver.find_element_by_xpath('//*[@id="bookPanel"]/li[6]')
 elem.click()
 
 This website makes me cry inside.'''
+
+'''
+#Goes back to search
+elem = driver.find_element_by_id("storySearchId")
+elem.clear()
+elem.send_keys(books[1])
+elem.send_keys(Keys.ENTER)
+elem.send_keys(Keys.TAB*2, Keys.ENTER)'''
+
+
+elem = driver.find_element_by_class_name("left")
+elem.send_keys(Keys.ENTER)
+'''
+elem = driver.find_element_by_class_name("back")
+elem.click()
+
+elem = driver.find_element_by_class_name("back.link.icon-only")
+elem.click()'''
 
 #//TODO Open FF profile that will put in correct folder
 #//Loop for every book
